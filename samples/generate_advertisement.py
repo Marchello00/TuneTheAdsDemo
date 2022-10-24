66f80ff0aaa05dbc4ff4dd7d4d7211a7d4c45ee1
Mark Nagovitsin <nagovitsin.ms@phystech.edu>
2022-10-24 13:58:21 +0300

    more samples

--- samples/generate_advertisement.py
+++ samples/generate_advertisement.py
@@ -1,11 +1,19 @@
 samples = [
-    'https://www.northstardubai.com/',
-    'https://www.gitex.com/',
+    # 'https://www.northstardubai.com/',
+    # 'https://www.gitex.com/',
     'https://stackoverflow.com/',
     'https://www.airbnb.com',
     'https://www.emirates.com/ae/english/',
-    'https://www.dubai-metro.me/',
+    # 'https://www.dubai-metro.me/',
     'https://www.python.org/',
     # 'http://netflix.com/',
-    'https://www.imdb.com/'
+    'https://www.imdb.com/',
+    'https://www.thecoffeemaven.com/coffee-roasters/washington',
+    'https://leeverslocavore.com/',
+    'https://lighthousewood.com/pages/custom-tables-nyc',
+    'https://www.cars.com/shopping/dallas-tx/',
+    'https://www.amazon.com/Philips-Norelco-Multigroom-attachment-MG3750/dp/B01K1HPA60/ref=sr_1_16?pd_rd_r=70ef1263-110e-4517-b4d8-64ac4d8e8c2b&pd_rd_w=sNRUA&pd_rd_wg=0bgg3&pf_rd_p=619b2b85-1519-44b0-b10f-193bd022e08c&pf_rd_r=3JA6PNN0SRZMQ9V9QQBN&qid=1666607559&s=beauty-intl-ship&sr=1-16',
+    'https://graceandthorn.com/',
+    'https://www.arethusafarm.com/',
+    'https://www.alemarcheese.com/'
 ]
